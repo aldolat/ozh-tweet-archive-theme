eda904b704a945f82234efcf5347a30f1bfa666b
aldolat <aldolat@gmail.com>
2014-04-24 22:36:24 +0200

    Fixed header.php

--- header.php
+++ header.php
@@ -15,7 +15,7 @@
 
 <div id="stats-wrap">
     <ul id="stats-header">
-        <li id="tiny-name">Ozh <a href="https://twitter.com/aldolat">@aldolat</a></li>
+        <li id="tiny-name">Aldo <a href="https://twitter.com/aldolat">@aldolat</a></li>
         <li><span class="what">tweets</span><span class="much"><?php do_action('ozh_ta_total_tweets'); ?></span></li>
         <li><span class="what">following</span><span class="much"><?php do_action('ozh_ta_total_following'); ?> <a href="https://twitter.com/aldolat/following"><i class="fa fa-caret-right"></i></a></span></li>
         <li><span class="what">followers</span><span class="much"><?php do_action('ozh_ta_total_followers'); ?> <a href="https://twitter.com/aldolat/followers"><i class="fa fa-caret-right"></i></a></span></li>
