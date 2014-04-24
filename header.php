3b8d8d34effd658fb5a49e83e172c09573e0cb68
aldolat <aldolat@gmail.com>
2014-04-24 21:19:23 +0200

    Customized header.php

--- header.php
+++ header.php
@@ -15,11 +15,11 @@
 
 <div id="stats-wrap">
     <ul id="stats-header">
-        <li id="tiny-name">Ozh <a href="https://twitter.com/ozh">@ozh</a></li>
+        <li id="tiny-name">Ozh <a href="https://twitter.com/aldolat">@aldolat</a></li>
         <li><span class="what">tweets</span><span class="much"><?php do_action('ozh_ta_total_tweets'); ?></span></li>
-        <li><span class="what">following</span><span class="much"><?php do_action('ozh_ta_total_following'); ?> <a href="https://twitter.com/ozh/following"><i class="fa fa-caret-right"></i></a></span></li>
-        <li><span class="what">followers</span><span class="much"><?php do_action('ozh_ta_total_followers'); ?> <a href="https://twitter.com/ozh/followers"><i class="fa fa-caret-right"></i></a></span></li>
-        <li><span class="what">listed</span><span class="much"><?php do_action('ozh_ta_total_listed'); ?> <a href="https://twitter.com/ozh/memberships"><i class="fa fa-caret-right"></i></a></span></li>
+        <li><span class="what">following</span><span class="much"><?php do_action('ozh_ta_total_following'); ?> <a href="https://twitter.com/aldolat/following"><i class="fa fa-caret-right"></i></a></span></li>
+        <li><span class="what">followers</span><span class="much"><?php do_action('ozh_ta_total_followers'); ?> <a href="https://twitter.com/aldolat/followers"><i class="fa fa-caret-right"></i></a></span></li>
+        <li><span class="what">listed</span><span class="much"><?php do_action('ozh_ta_total_listed'); ?> <a href="https://twitter.com/aldolat/memberships"><i class="fa fa-caret-right"></i></a></span></li>
     </ul>
 </div>
 
@@ -32,17 +32,19 @@
 <!-- !BEGIN #user -->
 <div id="user">
     <div id="user-pic">
-        <a href="https://twitter.com/ozh"  class="avatar"><zimg src="<?php bloginfo('template_url'); ?>/img/new-profile.jpg" alt="Ozh" /></a>
+        <a href="https://twitter.com/aldolat"  class="avatar"><zimg src="<?php bloginfo('template_url'); ?>/img/new-profile.jpg" alt="aldolat" /></a>
     </div>
     <div id="user-info">
         <ul>
-            <li class="user-realname">Ozh RICHARD</li>
-            <li class="username"><i class="fa fa-twitter"></i>@<a href="https://twitter.com/ozh">ozh</a></li>
-            <li class="username"><i class="fa fa-github-alt"></i>@<a href="https://github.com/ozh">ozh</a></li>
-            <li class="username"><i class="fa fa-facebook-square"></i><a href="https://www.facebook.com/ozh.richard">ozh.richard</a></li>
-            <li class="user-loc"><i class="fa fa-map-marker"></i>Nantes, FR</li>
-            <li class="user-url"><i class="fa fa-link"></i><a href="http://ozh.org">ozh.org</a></li>
-            <li class="user-url"><i class="fa fa-link"></i><a href="http://planetozh.com">planetozh.com</a></li>
+            <li class="user-realname">Aldo Latino</li>
+            <li class="username"><i class="fa fa-twitter"></i>@<a href="https://twitter.com/aldolat">aldolat</a></li>
+            <li class="username"><i class="fa fa-github-alt"></i>@<a href="https://github.com/aldolat">aldolat</a></li>
+            <li class="username"><i class="fa fa-facebook-square"></i><a href="https://www.facebook.com/aldo.latino">aldo.latino</a></li>
+            <li class="user-loc"><i class="fa fa-map-marker"></i>Augusta, IT</li>
+            <li class="user-url"><i class="fa fa-link"></i><a href="http://www.aldolat.it">www.aldolat.it</a></li>
+            <li class="user-url"><i class="fa fa-link"></i><a href="http://tweets.aldolat.it">tweets.aldolat.it</a></li>
+            <li class="user-url"><i class="fa fa-link"></i><a href="http://bookmarks.aldolat.it">bookmarks.aldolat.it</a></li>
+            <li class="user-url"><i class="fa fa-link"></i><a href="http://dev.aldolat.it">dev.aldolat.it</a></li>
         </ul>
     </div>
 </div>
