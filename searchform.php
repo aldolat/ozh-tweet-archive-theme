c01f36ebf5a319ea7f62e4e6995e2682b9d6dcd6
aldolat <aldolat@gmail.com>
2014-05-16 20:37:58 +0200

    Fixed 404 error while searching

--- searchform.php
+++ searchform.php
@@ -1,3 +1,3 @@
-<form method="get" id="searchform" action="/tweets/">
+<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
   <input type="text" name="s" id="s" value="<?php the_search_query(); ?>" class="txt" /><input type="submit" id="searchsubmit" value="&nbsp;" class="btn" />
 </form>
